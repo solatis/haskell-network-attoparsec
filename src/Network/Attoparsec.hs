a942c56b05396da52d7cab10464a6ce103f8e832
Leon Mergen <leon@solatis.com>
2015-02-17 14:59:41 +0700

    Improves documentation and moves some code around

--- src/Network/Attoparsec.hs
+++ src/Network/Attoparsec.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE OverloadedStrings #-}
 
--- | Utility functions for running a parser against a socket
+-- | Utility functions for running a parser against a socket, without the need
+--   of a bigger framework such as Pipes or Conduit.
 
 module Network.Attoparsec (parseMany, parseOne) where
 
@@ -12,20 +13,73 @@ import qualified Network.Socket             as NS
 import qualified Network.Socket.ByteString  as NSB
 import qualified Data.Attoparsec.ByteString as Atto
 
--- | The parsing continuation form of a "Data.Attoparsec" parser.
+-- | The parsing continuation form of a "Data.Attoparsec" parser. This is
+--   typically created by running the attoparsec "parse" function:
+--
+--   @
+--     createParser = AttoParsec.parse myParser
+--   @
 type ParseC a = BS.ByteString -> Atto.Result a
 
 -- | The type of parsing to perform, greedy or non-greedy
 data ParseMode = Single | Many
 
--- | Consumes input from socket and attempts to parse a structure. This function
---   will terminate if one of three conditions is met:
+-- | Consumes input from socket and attempts to parse as many objects from the
+--   socket as possible. Use this function only when you expect more than one
+--   parse operation to succeed.
 --
---   * the parser has completed succesfully;
---   * the parser failed with invalid data;
---   * the connection is closed.
+--   The function is continuation based, so you must provide the next parser
+--   state in successing calls as follows:
 --
---   Depending upon the available data on the socket, this function might block.
+--   > doParse sock = do
+--   >   (p1, xs1) <- parseMany sock (AttoParsec.parse myParser) (AttoParsec.parse myParser)
+--   >   (_,  xs2) <- parseMany sock (AttoParsec.parse myPArser) p1
+--   >   return (xs1 ++ xs2)
+--
+--   For more usage examples, see the test directory.
+parseMany :: ( MonadIO m
+             , MonadError String m
+             , Show a)
+          => NS.Socket         -- ^ Socket to read data from
+          -> ParseC a          -- ^ Initial parser state
+          -> ParseC a          -- ^ Continuation parser state
+          -> m (ParseC a, [a]) -- ^ Next parser state with parsed values
+parseMany s p0 pCur = do
+  buf <- liftIO $ NSB.recv s 4096
+  (p1, xs) <- parseBuffer p0 Many buf pCur
+  return (p1, xs)
+
+-- | Similar to parseMany, but assumes that there will only be enough data for a
+--   single succesful parse on the socket, and guarantees that exactly one item
+--   will be parsed.
+--
+--   __Warning:__ this function will /not/ work correctly when input data is
+--   pipelined. The parser might consume more data than required from the socket,
+--   or a partial second object is parsed, and the parser state and buffer will
+--   be discarded.
+--
+--  The is typically used as follows:
+--
+--  > doParse sock = parseOne sock (AttoParsec.parse myParser)
+parseOne :: ( MonadIO m
+            , MonadError String m
+            , Show a)
+         => NS.Socket -- ^ Socket to read data from
+         -> ParseC a  -- ^ Initial parser state
+         -> m a       -- ^ Parsed value
+parseOne s p0 = do
+  buf <- liftIO $ NSB.recv s 4096
+  (p1, value) <- parseBuffer p0 Single buf p0
+
+  case value of
+   -- We do not yet have enough data for a single item, let's request more
+   []  -> parseOne s p1
+   [x] -> return x
+
+   -- This is an internal error, since it means our single-object parser
+   -- returned multiple objects.
+   _   -> error ("More than one element parsed: " ++ show value)
+
 parseBuffer :: ( MonadIO m
                , MonadError String m
                , Show a)
@@ -62,43 +116,3 @@ parseBuffer p0 mode =
                      return (p1, v : xs)
 
   in next
-
--- | Incrementally reads data from socket and parses as many objects as possible
-parseMany :: ( MonadIO m
-             , MonadError String m
-             , Show a)
-          => NS.Socket         -- ^ Socket to read data from
-          -> ParseC a          -- ^ Initial parser state
-          -> ParseC a          -- ^ Continuation parser state
-          -> m (ParseC a, [a]) -- ^ Next parser state with parsed values
-parseMany s p0 pCur = do
-  buf <- liftIO $ NSB.recv s 4096
-  (p1, xs) <- parseBuffer p0 Many buf pCur
-  return (p1, xs)
-
--- | Similar to parseMany, but assumes that there will only be enough data for a
---   single succesful parse on the socket, and guarantees that exactly one item
---   will be parsed.
---
---   __Warning:__ this function will /not/ work correctly when input data is
---   pipelined. The parser might consume more data than required from the socket,
---   or a partial second object is parsed, and the parser state and buffer will
---   be discarded.
-parseOne :: ( MonadIO m
-            , MonadError String m
-            , Show a)
-         => NS.Socket -- ^ Socket to read data from
-         -> ParseC a  -- ^ Initial parser state
-         -> m a       -- ^ Parsed value
-parseOne s p0 = do
-  buf <- liftIO $ NSB.recv s 4096
-  (p1, value) <- parseBuffer p0 Single buf p0
-
-  case value of
-   -- We do not yet have enough data for a single item, let's request more
-   []  -> parseOne s p1
-   [x] -> return x
-
-   -- This is an internal error, since it means our single-object parser
-   -- returned multiple objects.
-   _   -> error ("More than one element parsed: " ++ show value)
